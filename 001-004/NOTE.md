dc9a3eb34b65709d5bdede247fdf9c72b0575efb
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2026-01-22 13:01:35 +0900

    backup

---
+++ 001-004/NOTE.md
@@ -0,0 +1,7 @@
+# 001-004
+
+## vulkan
+## vcpkg
+``` sh
+git clone https://github.com/microsoft/vcpkg.git
+```
---
+++ 001-004/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <vulkan/vulkan.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
