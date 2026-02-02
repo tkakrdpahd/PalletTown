2d2b53e0fd14bf1ce88838cf3b68195be28aa928
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2026-02-02 09:38:32 +0900

    Update 001-004

--- 001-004/main.c
+++ 001-004/main.c
@@ -2,7 +2,67 @@
 #include <SDL2/SDL.h>
 #include <vulkan/vulkan.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+#define ProjectName "Pallet Town"
+
+typedef struct {
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debug_messenger;
+    VkPhysicalDevice physical_device;
+    VkDevice device;
+    VkSurfaceKHR surface;
+} VulkanContext;
+
+void init_vulkan(VulkanContext* ctx) {
+    VkApplicationInfo appInfo = {
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = ProjectName,
+        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName = "No Engine",
+        .engineVersion = VK_MAKE_VERSION(1, 0, 0),
+        .apiVersion = VK_API_VERSION_1_0,
+    };
+
+    VkInstanceCreateInfo createInfo = {
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &appInfo,
+    };
+
+    vkCreateInstance(&createInfo, NULL, &ctx->instance);
+}
+
+void init_sdl() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+
+    SDL_Window* window = SDL_CreateWindow(
+		ProjectName,
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		800,
+		600,
+		window_flags
+	);
+
+    int running = 1;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    init_sdl();
+
+    VulkanContext vk_ctx;
+    init_vulkan(&vk_ctx);
+
     return 0;
 }
